1a5d072e8fca515a84def328b3f80add2ae03e18
Ekep Obasi <ekepobasi@gmail.com>
2023-08-19 07:20:02 +0100

    initial project setup
    - client site is setup with next js and typescript
    - server side is setup with express js and typescript

---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1 className="text-xl text-red-300">Homely - A house rental application.</h1>
+    </main>
+  );
+}

03c5a943998e532eb44697807a45acc29619ee65
Ekep Obasi <ekepobasi@gmail.com>
2023-08-28 22:49:16 +0100

    wrote helper function for getting current users location
    - the getLocation takes no parameters and returns a promise containing
      latitude and longitude

---
+++ client/src/app/utils/locations.tsx
@@ -0,0 +1,16 @@
+const getLocation = () => {
+  const promise = new Promise((res, rej) => {
+    navigator.geolocation?.getCurrentPosition(res, rej);
+  });
+
+  return promise.then((pos: any) => {
+    console.log(pos.coords.latitude, pos.coords.longitude);
+
+    return {
+      lat: pos.coords.latitude,
+      lng: pos.coords.longitude,
+    };
+  });
+};
+
+export default getLocation;

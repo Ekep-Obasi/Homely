e505f01149fedbce5289d6c273f39eb1870fec34
Ekep Obasi <ekepobasi@gmail.com>
2023-08-29 20:59:16 +0100

    feat: created the products tabs
    - to display the house based on tab categories

---
+++ client/src/app/components/product-tabs.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { TabsContent } from "./ui/tabs";
+import { AppContext } from "../context/app-context";
+import GoogleMapWrapper from "./GoogleMap/GoogleMap";
+
+interface listProps {
+  image: string;
+  address: string;
+  location: string;
+  availability: "AVAILABLE" | "NOT AVAILABLE";
+  rooms: number;
+  bathrooms: number;
+  cost: string;
+  liked?: boolean;
+  area: number;
+}
+
+interface Props {
+  list: listProps[];
+  Component: any;
+  value: string;
+}
+
+export default function ProductTabs(props: Props) {
+  const { toggleShowMap }: any = React.useContext(AppContext);
+
+  return (
+    <TabsContent
+      value={props.value}
+      className="w-full flex py-0 space-x-2 items-center px-2"
+    >
+      <section className="h-[620px] w-full flex gap-x-2 gap-y-4 items-center flex-wrap overflow-y-auto mx-auto justify-center">
+        {props.list.length !== 0 ? (
+          props.list.map((payload: any, id: any) => (
+            <props.Component key={id} {...payload} />
+          ))
+        ) : (
+          <>No Available products yet</>
+        )}
+      </section>
+      {toggleShowMap && (
+        <div className="bg-primary w-full h-full">
+          <GoogleMapWrapper />
+        </div>
+      )}
+    </TabsContent>
+  );
+}

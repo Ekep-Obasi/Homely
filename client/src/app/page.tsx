c6f48ff32678831e041a76932938045fdff0b388
Ekep Obasi <ekepobasi@gmail.com>
2023-08-29 03:00:01 +0100

    feat:started building the landing page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import { Button } from "./components/ui/button";
 
 export default function Home() {
   return (
     <main className="">
-      <h1 className="text-xl text-red-300">Homely - A house rental application.</h1>
+      <h1 className="text-xl text-red-300">
+        Homely - A house rental application.
+      </h1>
+      <Button>Click me</Button>
     </main>
   );
 }

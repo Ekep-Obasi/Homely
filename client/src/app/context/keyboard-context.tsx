e8d5c9645215f402318a0912bb613c80ba9d5ff0
Ekep Obasi <ekepobasi@gmail.com>
2023-08-29 20:57:44 +0100

    feat:created keyboard context
    - it exposes the message state of the text area
    - it also exposes the showPicker state that is responsible for displaying the EmojiPicker

---
+++ client/src/app/context/keyboard-context.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const KeyBoardContext = React.createContext({});
+
+export default function KeyBoardProvider({ children }: Props) {
+  const [showPicker, setShowPicker] = useState(false);
+  const [message, setMessage] = useState("");
+
+  return (
+    <KeyBoardContext.Provider
+      value={{ showPicker, setShowPicker, message, setMessage }}
+    >
+      {children}
+    </KeyBoardContext.Provider>
+  );
+}

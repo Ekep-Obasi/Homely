31980e0dd6501700a20012d9c83b27439f0f3a6a
Ekep Obasi <ekepobasi@gmail.com>
2023-08-29 20:39:39 +0100

    created loading, layout & landing page for chat section

---
+++ client/src/app/chat/loading.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export default function ChatLoading () {
+  return(
+    <div>loading...</div>
+  )
+}
--- client/src/app/chat/page.tsx
+++ client/src/app/chat/page.tsx
@@ -1,5 +1,39 @@
-export default function ChatPage () {
+"use client";
+
+import * as React from "react";
+import { Tabs, TabsList, TabsTrigger } from "../components/ui/tabs";
+import { chats } from "../constants/chats";
+import { ChatTab } from "../components/chat-tabs";
+import { TabsContent } from "@radix-ui/react-tabs";
+import { Separator } from "../components/ui/separator";
+import ChatTabHeader from "../components/chat-tabs-header";
+import GeneralChat from "../components/general-chat-section";
+
+export default function ChatPage() {
   return (
-    <div>Chat Page</div>
-  )
+    <Tabs
+      defaultValue="Homely"
+      className="w-full flex bg-primary overflow-hidden"
+    >
+      <TabsList className="flex-col justify-between bg-primary gap-y-2 min-h-screen overflow-auto w-1/4 min-w-[320px]">
+        <ChatTabHeader />
+        {chats.map((payload, i) => (
+          <TabsTrigger className="w-full" key={payload._id} value={payload._id}>
+            <ChatTab {...payload} />
+          </TabsTrigger>
+        ))}
+      </TabsList>
+      <Separator orientation="vertical" />
+
+      {chats.map((data) => (
+        <TabsContent
+          className="w-3/4 max-h-screen bg-background overflow-y-hidden"
+          key={data._id}
+          value={data._id}
+        >
+          <GeneralChat id={data._id} />
+        </TabsContent>
+      ))}
+    </Tabs>
+  );
 }

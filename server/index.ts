10db00828f6c5308e081745894a2241a9ec5f2b6
Ekep-Obasi <arreyettaekep@gmail.com>
2023-09-04 10:12:45 +0100

    chore: deleted useless constants - deleted PORTNUMBER and MONGO_DB_URI from the index.ts - setup the PropertyRouter as

--- server/index.ts
+++ server/index.ts
@@ -1,31 +1,32 @@
-import express, { Express } from 'express';
-import mongoose from 'mongoose';
-import dotEnv from 'dotenv';
-import { PropertyRouter } from './routes';
-import { UserRouter } from './routes/UserRoute';
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import dotEnv from "dotenv";
+import { PropertyRouter } from "./routes";
+import { UserRouter } from "./routes/UserRoute";
+import path from "path";
+import { MONGODB_URI, PORTNUMBER } from "./constant";
 dotEnv.config();
 
 const app: Express = express();
-const PORTNUMBER = process.env.PORT_NUMBER || 5000;
-const MONGODB_URI = process.env.MONGODB_CONNECTION_STRING || '';
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
-app.use('/api/v1/user', UserRouter)
-app.use('/api/v1/property', PropertyRouter)
+app.use("/api/v1/user", UserRouter);
+app.use("/api/v1/property", PropertyRouter);
 
 app.listen(PORTNUMBER, () => {
-  console.clear()
-  console.log(`server is running on port ${PORTNUMBER}`)
+  console.clear();
+  console.log(`server is running on port ${PORTNUMBER}`);
 });
 
 (async () => {
   try {
     await mongoose.connect(MONGODB_URI);
     console.log("connected to MongoDB succesfully!");
-  }
-  catch (error) {
+  } catch (error) {
     console.log(error);
   }
 })();

659f2cef6b64ddc8792dccf9e8c1079e5a0cccf3
Ekep-Obasi <arreyettaekep@gmail.com>
2023-09-04 10:38:20 +0100

    Added auth middleware to the protected user routes

--- server/routes/UserRoute.ts
+++ server/routes/UserRoute.ts
@@ -1,21 +1,25 @@
-import express from 'express';
-import { CreateUser, EditUserProfile, GetAllUsers, GetUserID, GetUserProfile, UserLogin } from '../controllers';
-import AuthMiddleware from '../middleware/AuthMiddleware';
+import express from "express";
+import {
+  CreateUser,
+  EditUserProfile,
+  GetAllUsers,
+  GetUserID,
+  GetUserProfile,
+  PostUserReview,
+  UserLogin,
+} from "../controllers";
+import { AuthMiddleware } from "../middleware";
 
 const router = express.Router();
 
-router.post('/login', UserLogin);
-router.post('/', CreateUser);
+router.post("/login", UserLogin);
+router.post("/signup", CreateUser);
 
 router.use(AuthMiddleware);
-router.get('/profile', GetUserProfile);
-router.patch('/profile', EditUserProfile)
-router.get('/', GetAllUsers);
-router.get('/:id', GetUserID);
-
-
-
-
-
+router.get("/profile", GetUserProfile);
+router.patch("/profile/edit", EditUserProfile);
+router.get("/", GetAllUsers);
+router.get("/:id", GetUserID);
+router.post("/post/:id", PostUserReview);
 
 export { router as UserRouter };

2d528b1658dc6423050f957ee843491db79f6d36
Ekep-Obasi <arreyettaekep@gmail.com>
2023-09-04 10:21:17 +0100

    feat:configured the multer fileupload middleware - setup middleware to upload multiple images; max number of images to be uploaded is 10 - setup middleware to upload single images which is the user
    's avatar

---
+++ server/middleware/ImageUploadMiddleware.ts
@@ -0,0 +1,18 @@
+import multer from "multer";
+
+const imageStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now().toString() + "-" + file.originalname);
+  },
+});
+
+export const propertyImageListMiddleWare = multer({
+  storage: imageStorage,
+}).array("image_list", 10);
+
+export const profileImageMiddleWare = multer({ storage: imageStorage }).single(
+  "avatar"
+);

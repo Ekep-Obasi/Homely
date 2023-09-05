df8805e42ed79700393ffdc9858716242936ea2f
Ekep-Obasi <arreyettaekep@gmail.com>
2023-09-05 12:23:46 +0100

    seperation of database concerns from express app configuration concerns

--- server/index.ts
+++ server/index.ts
@@ -1,32 +1,17 @@
 import express, { Express } from "express";
-import mongoose from "mongoose";
-import dotEnv from "dotenv";
-import { PropertyRouter } from "./routes";
-import { UserRouter } from "./routes/UserRoute";
-import path from "path";
-import { MONGODB_URI, PORTNUMBER } from "./constant";
-dotEnv.config();
-
-const app: Express = express();
-
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/images", express.static(path.join(__dirname, "images")));
-
-app.use("/api/v1/user", UserRouter);
-app.use("/api/v1/property", PropertyRouter);
-
-app.listen(PORTNUMBER, () => {
-  console.clear();
-  console.log(`server is running on port ${PORTNUMBER}`);
-});
+import { PORTNUMBER } from "./constant";
+import dbConnection from "./services/Database";
+import App from "./services/ExpressApp";
 
 (async () => {
-  try {
-    await mongoose.connect(MONGODB_URI);
-    console.log("connected to MongoDB succesfully!");
-  } catch (error) {
-    console.log(error);
-  }
+  const app: Express = express();
+
+  await App(app);
+
+  await dbConnection();
+
+  app.listen(PORTNUMBER, () => {
+    console.clear();
+    console.log(`server is listening to port ${PORTNUMBER}`);
+  });
 })();

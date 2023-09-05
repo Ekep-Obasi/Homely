943089a78e77cb3f7de3793c4242bd460bdee282
Ekep-Obasi <arreyettaekep@gmail.com>
2023-09-05 12:22:03 +0100

    seperated the function for setting up mongodb and setting up the app - exported the an asynchronous function from the ExpressApp that setups up middleware for the express app - exported an asynchronouse function from the Database.ts file that does the mongodb connection

---
+++ server/services/Database.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import { MONGODB_URI } from "../constant";
+
+export default async () => {
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log("connected to MongoDB succesfully!");
+  } catch (error) {
+    console.log(error);
+  }
+};
